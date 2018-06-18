1de842ed0f8f8ef08b1c70b62354fbc27cfa9334
Joel Bennett <Jaykul@HuddledMasses.org>
2018-06-18 01:06:11 -0400

    Add a HostPreference to allow suppressing Write-Host
    Even though it won't stop PSCmdlet.WriteHost?

--- Source/Private/_init.ps1
+++ Source/Private/_init.ps1
@@ -1,6 +1,13 @@
 using namespace PoshCode.Pansies
 using namespace ColorMine.ColorSpaces
 
+# On first import, if HostPreference doesn't exist, set it and strongly type it
+if(!(Test-Path Variable:HostPreference) -or $HostPreference -eq $null) {
+    [System.Management.Automation.ActionPreference]$global:HostPreference = "Continue"
+}
+
+Set-Variable HostPreference -Description "Dictates the action taken when a host message is delivered" -Visibility Public -Scope Global
+
 if(-not $IsLinux -and -not $IsMacOS) {
     [PoshCode.Pansies.Console.WindowsHelper]::EnableVirtualTerminalProcessing()
 }
