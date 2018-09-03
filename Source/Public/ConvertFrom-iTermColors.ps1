c309a79c0affe9d1ddebfb94579819d0b408bfe4
Joel Bennett <Joel.Bennett@Questionmark.com>
2018-09-02 23:51:28 -0400

    Refactor ConvertFrom-iTermColors to use generic PList parser

--- Source/Public/ConvertFrom-iTermColors.ps1
+++ Source/Public/ConvertFrom-iTermColors.ps1
@@ -1,11 +1,11 @@
-﻿function ConvertFrom-iTermColors {
+function ConvertFrom-iTermColors {
     <#
         .SYNOPSIS
             Convert a .itermcolors XML file into a partial theme
         .DESCRIPTION
             Generate a PANSIES PowerShell theme from iTermColors.
 
-            For a collection of iTermColors files you can use, visit https://github.com/mbadolato/iTerm2-Color-Schemes
+            For a collection of iTermColors files you can use, visit https:#ithub.com/mbadolato/iTerm2-Color-Schemes
 
         .EXAMPLE
             ConvertFrom-iTermColors Argonaut
@@ -28,50 +28,76 @@
         [string]$Scope = "User"
     )
 
+    begin {
+        # In Windows Color Table order
+        $PListColorNames = @(
+            "Ansi 0 Color"   # DARK_BLACK
+            "Ansi 4 Color"   # DARK_BLUE
+            "Ansi 2 Color"   # DARK_GREEN
+            "Ansi 6 Color"   # DARK_CYAN
+            "Ansi 1 Color"   # DARK_RED
+            "Ansi 5 Color"   # DARK_MAGENTA
+            "Ansi 3 Color"   # DARK_YELLOW
+            "Ansi 7 Color"   # DARK_WHITE
+            "Ansi 8 Color"   # BRIGHT_BLACK
+            "Ansi 12 Color"  # BRIGHT_BLUE
+            "Ansi 10 Color"  # BRIGHT_GREEN
+            "Ansi 14 Color"  # BRIGHT_CYAN
+            "Ansi 9 Color"   # BRIGHT_RED
+            "Ansi 13 Color"  # BRIGHT_MAGENTA
+            "Ansi 11 Color"  # BRIGHT_YELLOW
+            "Ansi 15 Color"  # BRIGHT_WHITE
+        )
+    }
+
     process {
-        if (Test-Path $Theme) {
-            $ThemeFile = Get-Item $Theme
-        } else {
-            # Make sure that it ends in .itermcolors
-            $ThemeName = [IO.Path]::GetFileNameWithoutExtension($Theme) + ".itermcolors"
-
-            if(!($ThemeFile = Get-ChildItem -Filter $ThemeName -ErrorAction SilentlyContinue)) {
-                $ThemeFile = Get-ChildItem $(Get-ConfigurationPath -Scope $Scope) -Filter $ThemeName -Recurse -ErrorAction SilentlyContinue
-            }
+        if (!(Test-Path $Theme)) {
+            throw [System.IO.FileNotFoundException]::new("Palette file not found", $Theme)
         }
 
-        if(!(Test-Path $ThemeFile.FullName)) {
-            Write-Error "Cannot file '$($ThemeFile.FullName)'"
-        } else {
-            try {
-                $Palette = [PoshCode.Pansies.Parsers.PListParser]::Parse($ThemeFile.FullName);
-            }
-            catch [System.IO.FileNotFoundException]
-            {
-                $PSCmdlet.ThrowTerminatingError( [System.Management.Automation.ErrorRecord]::new($_, "PaletteFileNotFound", "ObjectNotFound", $ThemeFile.FullName));
-            }
-            catch
-            {
-                $Ex = if($_ -is [Exception]) { $_ } else { $_.Exception }
-                $PSCmdlet.ThrowTerminatingError( [System.Management.Automation.ErrorRecord]::new($Ex, "PaletteParseException", "ParserError", $ThemeFile.FullName));
-            }
-
-            if (!$Palette)
-            {
-                $PSCmdlet.ThrowTerminatingError( [System.Management.Automation.ErrorRecord]::new(
-                    [System.IO.InvalidDataException]::new("Unknown Palette file format: '$($ThemeFile.FullName)'"),
-                     "PaletteParseFailure", "NotImplemented", $ThemeFile.FullName));
-            }
-            $ThemeOutput = @{
-                ConsoleColors = @($Palette[0..15].ForEach({$_.ToString()}))
-            }
+        if (!($pList = Import-PList $Theme)) {
+            return
         }
 
-        if ($Palette.Count -ge 18) {
-            $ThemeOutput['ConsoleBackground'] = $Palette[16].ToString()
-            $ThemeOutput['ConsoleForeground'] = $Palette[17].ToString()
+        $ThemeOutput = @{
+            ConsoleColors = @(
+                foreach($color in $PListColorNames) {
+                    if(!$pList[$color]) {
+                        Wait-Debugger
+                        Write-Warning "Missing color $color"
+                        $null
+                    } else {
+                        ConvertToCssColor $pList[$color]
+                    }
+                }
+            )
+        }
+        if ($pList.ContainsKey("Foreground Color") -and $pList.ContainsKey("Background Color")) {
+            $ThemeOutput.ConsoleForeground = ConvertToCssColor $pList["Foreground Color"]
+            $ThemeOutput.ConsoleBackground = ConvertToCssColor $pList["Background Color"]
+        }
+        if ($pList.ContainsKey("Bold Color")) {
+            if (!$ThemeOutput['PSReadLine']) {
+                $ThemeOutput['PSReadLine'] = @{}
+            }
+            if (!$ThemeOutput['PSReadLine']['Colors']) {
+                $ThemeOutput['PSReadLine']['Colors'] = @{}
+            }
+            $ThemeOutput['PSReadLine']['Colors']['Emphasis'] = ConvertToCssColor $pList["Bold Color"]
+        }
+        if ($pList.ContainsKey("Selection Color")) {
+            if (!$ThemeOutput['PSReadLine']) {
+                $ThemeOutput['PSReadLine'] = @{}
+            }
+            if (!$ThemeOutput['PSReadLine']['Colors']) {
+                $ThemeOutput['PSReadLine']['Colors'] = @{}
+            }
+            $ThemeOutput['PSReadLine']['Colors']['Selection'] = ConvertToCssColor $pList["Selection Color"]
+        }
+        if(!($Name = $pList["name"])) {
+            $Name = ([IO.Path]::GetFileNameWithoutExtension($Theme))
         }
 
-        $ThemeOutput | ExportTheme -Name $ThemeFile.BaseName -Passthru:$Passthru -Scope:$Scope -Force:$Force
+        $ThemeOutput | ExportTheme -Name $Name -Passthru:$Passthru -Scope:$Scope -Force:$Force
     }
 }
