79c58e990e7e9903e26874ca30688f04e2748287
Joel Bennett <Jaykul@HuddledMasses.org>
2021-07-16 04:13:01 -0400

    Add emoji and nerdfonts
    But currently, don't turn them on by default
    +semver:feature

--- Source/Assembly/Entities.cs
+++ Source/Assembly/Entities.cs
@@ -1,77 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 using System.Net;
 using System.Text;
 
 namespace PoshCode.Pansies
 {
-    public static class Entities
+    public static partial class Entities
     {
+        public static bool EnableEmoji = false;
+        public static bool EnableNerdFonts = false;
         private static readonly char[] _entityEndingChars = new char[] { ';', '&' };
 
-        public static Dictionary<string, string> EscapeSequences = new Dictionary<string, string>
+        public static SortedList<string, string> EscapeSequences = new SortedList<string, string>(StringComparer.InvariantCultureIgnoreCase)
         {
-            { "Esc", "\u001B[" },
-            { "Store", "\u001B[s" },
-            { "Recall", "\u001B[u" },
-            { "Clear", "\u001B[0m" }
+            ["Clear"] = "\u001B[0m",
+            ["Esc"] = "\u001B[",
+            ["Store"] = "\u001B[s",
+            ["Recall"] = "\u001B[u",
         };
 
-        public static Dictionary<string, string> ExtendedCharacters = new Dictionary<string, string>
+        public static SortedList<string, string> ExtendedCharacters = new SortedList<string, string>(StringComparer.InvariantCultureIgnoreCase)
         {
-            { "ColorSeparator", "\u258C"}, // ▌
-            { "ReverseColorSeparator", "\u2590"}, // ▐
-            { "Separator", "\u25BA"}, // ►
-            { "ReverseSeparator", "\u25C4"}, // ◄
-            { "Branch", "\ue0a0"}, // Branch symbol
-            { "Lock", "\ue0a2"}, // Padlock
-            { "Gear", "\u26ef"}, // The settings icon, I use it for debug
-            { "Power", "\u26a1"}, // The Power lightning-bolt icon
+            ["Branch"] = "\ue0a0",                // Branch symbol
+            ["ColorSeparator"] = "\u258C",        // ▌
+            ["Gear"] = "\u26ef",                  // The settings icon, I use it for debug
+            ["Lock"] = "\ue0a2",                  // Padlock
+            ["Power"] = "\u26a1",                 // The Power lightning-bolt icon
+            ["ReverseColorSeparator"] = "\u2590", // ▐
+            ["ReverseSeparator"] = "\u25C4",      // ◄
+            ["Separator"] = "\u25BA",             // ►
         };
 
         public static string Decode(string value)
         {
-            var output = new StringWriter(CultureInfo.InvariantCulture);
-
-            int l = value.Length;
-            for (int i = 0; i < l; i++)
+            var output = new StringBuilder();
+            if (value.IndexOf('&') == -1)
             {
-                char ch = value[i];
+                return value;
+            }
 
-                if (ch == '&')
+            int end = 0, start = 0;
+            while((start = value.IndexOf('&', end)) != -1)
+            {
+                string result;
+                output.Append(value.Substring(end, start - end));
+                // We found a '&'. Now look for the next ';' or '&'.
+                end = value.IndexOfAny(_entityEndingChars, start + 1);
+                // If we find another '&' then this is not an entity!
+                if (end > 0 && value[end] == ';')
                 {
-                    // We found a '&'. Now look for the next ';' or '&'.
-                    // If we find another '&' then this is not an entity,
-                    int index = value.IndexOfAny(_entityEndingChars, i + 1);
-                    if (index > 0 && value[index] == ';')
+                    string entity = value.Substring(start + 1, end - start - 1);
+                    end++;
+
+                    if (EscapeSequences.TryGetValue(entity, out result))
                     {
-                        string entity = value.Substring(i + 1, index - i - 1);
-                        i = index;
-                        if (EscapeSequences.ContainsKey(entity))
-                        {
-                            output.Write(EscapeSequences[entity]);
-                            continue;
-                        }
-                        else if (ExtendedCharacters.ContainsKey(entity))
-                        {
-                            output.Write(ExtendedCharacters[entity]);
-                            continue;
-                        }
-                        else
-                        {
-                            output.Write('&');
-                            output.Write(entity);
-                            output.Write(';');
-                            continue;
-                        }
+                        output.Append(result);
+                    }
+                    else if (ExtendedCharacters.TryGetValue(entity, out result))
+                    {
+                        output.Append(result);
+                    }
+                    else if (EnableEmoji && Emoji.TryGetValue(entity, out result))
+                    {
+                        output.Append(result);
+                    }
+                    else if (EnableNerdFonts && NerdFonts.TryGetValue(entity, out result))
+                    {
+                        output.Append(result);
+                    }
+                    else
+                    {
+                        output.Append('&');
+                        output.Append(entity);
+                        output.Append(';');
+                        continue;
                     }
                 }
-
-                output.Write(ch);
             }
-            
+            // make sure we don't loose anything off the end
+            output.Append(value.Substring(end, value.Length - end));
+
             value = WebUtility.HtmlDecode(output.ToString());
             return value;
         }
